73dfc321d9bd8670b72a697ee8539d724601edea
KoryakinaAlina <Tyuiu.KoryakinaAE@outlook.com>
2025-09-11 14:07:11 +0500

    Task2

---
+++ Tyuiu.KoryakinaAE.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.KoryakinaAE.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.KoryakinaAE.Sprint0.Task2.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Вызов класса DataService и метода GetMassage 
+            //из библиотеки Tyuiu.KoryakinaAE.Sprint0.Task2.V0.Lib
+            Console.WriteLine(DataService.GetMassage("Алина"));
+            Console.ReadKey();
+            
+        }
+    }
+}
