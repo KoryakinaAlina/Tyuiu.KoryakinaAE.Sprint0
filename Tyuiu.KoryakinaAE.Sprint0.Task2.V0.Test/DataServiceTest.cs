ccc57f05746649049d7d8b818d0a61a6d3e58293
KoryakinaAlina <Tyuiu.KoryakinaAE@outlook.com>
2025-09-11 13:27:26 +0500

    Task2

---
+++ Tyuiu.KoryakinaAE.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.KoryakinaAE.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.KoryakinaAE.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMassageValid()
+        {
+            // Область создания методов тестирования, методов из библиотеки
+            var name = "Алина";
+            var res = DataService.GetMassage(name);
+
+            //Вызываем класс Assert и метод AreEual
+            Assert.AreEqual("Привет..., Алина", res);
+        }
+    }
+}
